effb1dbf59db9bf50d2cc0dcc9543594288211e5
Lia Crupi <lia.crupi@Lias-MacBook-Pro.local>
2026-04-29 20:18:48 +0300

    Add homepage with Sanity connection

---
+++ sanity/schemaTypes/homepage.ts
@@ -0,0 +1,17 @@
+export default {
+  name: 'homepage',
+  title: 'Homepage',
+  type: 'document',
+  fields: [
+    {
+      name: 'heroHeading',
+      title: 'Hero Heading',
+      type: 'string',
+    },
+    {
+      name: 'heroButtonText',
+      title: 'Hero Button Text',
+      type: 'string',
+    },
+  ],
+}
